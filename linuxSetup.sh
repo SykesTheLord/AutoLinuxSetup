7e88a6421adbb4ac90275c98896d0444f3a2cf36
Jacob Sykes Bjerregaard <jasb@bunker-holding.com>
2025-02-25 12:20:23 +0100

    Create gitignore and start file

---
+++ linuxSetup.sh
@@ -0,0 +1,10 @@
+#
+sudo apt update && sudo apt upgrade -y  # Update and upgrade the system
+sudo apt-get install clangd-12 -y # Install Clangd for C and C++ development
+sudo apt-get install -y dotnet-sdk-8.0
+sudo apt install default-jre -y
+sudo apt install openjdk-8-jre -y
+
+
+wget -O - https://raw.githubusercontent.com/SykesTheLord/NeoVimConfig/refs/heads/main/NvimSetup.sh | sh
+
