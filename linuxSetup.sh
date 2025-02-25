7e64c8e0cd02e9551608a3193a8bf8f998b37d7a
Jacob Sykes Bjerregaard <jasb@bunker-holding.com>
2025-02-25 13:57:16 +0100

    Flesh out the rest of the script

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -1,10 +1,161 @@
-#
-sudo apt update && sudo apt upgrade -y  # Update and upgrade the system
+#!/bin/bash
+
+DISTRO=$(lsb_release -is 2>/dev/null)
+
+
+if [[ "$DISTRO" == "Ubuntu" ]]; then
+    # Install pre-requisite packages.
+    sudo apt-get install -y wget apt-transport-https software-properties-common
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # Install Docker elements
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    # Get the version of Ubuntu
+    source /etc/os-release
+
+    # Download the Microsoft repository keys
+    wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+
+    # Register the Microsoft repository keys
+    sudo dpkg -i packages-microsoft-prod.deb
+
+    # Delete the Microsoft repository keys file
+    rm packages-microsoft-prod.deb
+
+    # Update the list of packages after we added packages.microsoft.com
+    sudo apt-get update
+
+    ###################################
+    # Install PowerShell
+    sudo apt-get install -y powershell
+
+elif [["$DISTRO" == "Debian"]] 
+    # Install pre-requisite packages.
+    sudo apt-get install -y wget
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # Install Docker elements
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    # Get the version of Debian
+    source /etc/os-release
+
+    # Download the Microsoft repository GPG keys
+    wget -q https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb
+
+    # Register the Microsoft repository GPG keys
+    sudo dpkg -i packages-microsoft-prod.deb
+
+    # Delete the Microsoft repository GPG keys file
+    rm packages-microsoft-prod.deb
+
+    # Update the list of packages after we added packages.microsoft.com
+    sudo apt-get update
+
+    ###################################
+    # Install PowerShell
+    sudo apt-get install -y powershell
+else
+    echo "No supported Distro for install found"
+    exit 1
+fi
+
+if ! getent group docker > /dev/null; then
+    echo "Group 'docker' does not exist. Creating it now..."
+    sudo groupadd docker
+    echo "Group 'docker' created successfully."
+else
+    echo "Group 'docker' already exists."
+fi
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo apt update # Update the system
+sudo apt upgrade -y  # Upgrade the system
 sudo apt-get install clangd-12 -y # Install Clangd for C and C++ development
-sudo apt-get install -y dotnet-sdk-8.0
-sudo apt install default-jre -y
-sudo apt install openjdk-8-jre -y
+sudo apt-get install -y dotnet-sdk-8.0 # Install dotnet
+sudo apt install default-jre -y # Install openjdk jre
+sudo apt install openjdk-8-jre -y # Install openjdk 8 jre
 
+## Install Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-commonv
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform -y
 
+# Fetch the latest Bicep CLI binary
+curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
+# Mark it as executable
+chmod +x ./bicep
+# Add bicep to  PATH 
+sudo mv ./bicep /usr/local/bin/bicep
+
+# Install and setup Neovim
 wget -O - https://raw.githubusercontent.com/SykesTheLord/NeoVimConfig/refs/heads/main/NvimSetup.sh | sh
 
+# Download and install fnm:
+curl -o- https://fnm.vercel.app/install | bash
+
+# Download and install Node.js:
+fnm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.14.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
+
+## Install Zsh and set it as default shell.
+sudo apt install zsh -y
+chsh -s $(which zsh)
+
+# Install Oh-My-Zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Oh-My-Zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+wget -O .zshrc https://raw.githubusercontent.com/SykesTheLord/AutoLinuxSetup/refs/heads/main/.zshrc
+
+# Refresh Zsh
+source .zshrc
+
+## Install Terraform autocomplete
+terraform -install-autocomplete
+
+# Refresh Zsh
+source .zshrc
+
+pwsh -Command "Install-Module -Name Az -Repository PSGallery -Force"
