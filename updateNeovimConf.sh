b2570a9728f228f7a80d2958231c196ccb158ea4
Jacob Sykes <jacob@sykes-sec.com>
2025-03-25 20:04:21 +0100

    Updated updateNeovimConf.sh

--- updateNeovimConf.sh
+++ updateNeovimConf.sh
@@ -1,2 +1,3 @@
-wget -O ~/.config/nvim/init.vim https://raw.githubusercontent.com/SykesTheLord/NeoVimConfig/refs/heads/main/init.vim
+rm -rf ~/.config/nvim/*
+git clone https://github.com/SykesTheLord/NeoVimConfig.git ~/.config/nvim
 nvim +PlugInstall
