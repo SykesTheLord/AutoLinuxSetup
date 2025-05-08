dbc42ce9e890ae6da62a23fc2d136edea70f3ed1
Jacob Sykes <jacob@sykes-sec.com>
2025-05-08 18:49:36 +0200

    Updated some stuff for hyprland

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -259,13 +259,25 @@ elif [ -f "/etc/arch-release" ]; then
         if ! command -v hyprland &> /dev/null; then
             print_message "Installing Hyprland"
             # Install required programs
-            sudo pacman -S --noconfirm swaync wireplumber pipewire qt6-wayland qt5-wayland kitty
+            sudo pacman -S --noconfirm swaync wireplumber pipewire qt6-wayland qt5-wayland kitty wl-clipboard gnome-keyring libsecret slurp grim imagemagick swappy
             yay -S hyprland-git
+            yay -S hyprlock-git
+            yay -S hyprpaper-git
+            yay -S hypridle-git
+            yay -S fastfetch
+            yay -S wofi
+            yay -S waybar
+            yay -S brightnessctl
             pacman -S --noconfirm uwsm
             # Enable services
             sudo systemctl enable swaync
             yay -S xdg-desktop-portal-hyprland-git
             # Get hyprland setup from dotfiles repo
+            git clone https://github.com/SykesTheLord/DotFiles.git
+            cd DotFiles
+            cd arch
+            cp .config ~/.config
+            cp .scripts ~/.scripts
         fi
     fi
 
