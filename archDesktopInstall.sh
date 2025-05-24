71ef59b4e47b96c9ad53aea7dff7ccb1fa3863dd
Jacob Sykes <jacob@sykes-sec.com>
2025-05-24 11:16:48 +0200

    Add noconfirm to yay packages

--- archDesktopInstall.sh
+++ archDesktopInstall.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 sudo pacman -S --noconfirm swaync wireplumber pipewire qt6-wayland qt5-wayland kitty wl-clipboard gnome-keyring libsecret slurp grim imagemagick swappy  spotify-launcher pavucontrol rofi blueman qt6ct bitwarden network-manager-applet nwg-look font-config rustc cargo ghostty linux-headers
-yay -S hyprland-git
-yay -S hyprlock-git
-yay -S hyprpaper-git
-yay -S hypridle-git
-yay -S fastfetch
-yay -S wofi
-yay -S waybar
-yay -S brightnessctl
+yay -S --noconfirm hyprland-git
+yay -S --noconfirm hyprlock-git
+yay -S --noconfirm hyprpaper-git
+yay -S --noconfirm hypridle-git
+yay -S --noconfirm fastfetch
+yay -S --noconfirm wofi
+yay -S --noconfirm waybar
+yay -S --noconfirm brightnessctl
 pacman -S --noconfirm uwsm
 # Enable services
 sudo systemctl enable swaync
-yay -S xdg-desktop-portal-hyprland-git
+yay -S --noconfirm xdg-desktop-portal-hyprland-git
 # Get hyprland setup from dotfiles repo
 git clone https://github.com/SykesTheLord/DotFiles.git
 cd DotFiles
 cd arch
 cp -r * ~/
 
-yay -S QGtk3Style
-yay -S themix-full-git
-yay -S eww
-yay -S discord_arch_electron
-yay -S hyprsysteminfo
-yay -S hyprcursor
-yay -S wlogout
-yay -S vmware-keymaps
-yay -S vmware-workstation
-yay -S nordzy-hyprcursors
-yay -S nordzy-cursors
+yay -S --noconfirm QGtk3Style
+yay -S --noconfirm themix-full-git
+yay -S --noconfirm eww
+yay -S --noconfirm discord_arch_electron
+yay -S --noconfirm hyprsysteminfo
+yay -S --noconfirm hyprcursor
+yay -S --noconfirm wlogout
+yay -S --noconfirm vmware-keymaps
+yay -S --noconfirm vmware-workstation
+yay -S --noconfirm nordzy-hyprcursors
+yay -S --noconfirm nordzy-cursors
